caf6c679beb8694d1b4e2de6a112349a374998e0
AkmElias <akmelias11@gmail.com>
2020-07-14 00:25:55 +0600

    Configure database
    - create user and role model using mongoose
    - connect to mongodb and create two important roles

--- fullstack_api/server.js
+++ fullstack_api/server.js
@@ -16,6 +16,51 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const dbConfig = require("./config/db.config");
+const db = require("./models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+//function to create 2 important rows in roles collection.
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to fullstack application." });
